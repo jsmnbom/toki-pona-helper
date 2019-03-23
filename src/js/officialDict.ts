b2fe11e7660569951046c4b3bb88d68e037f1154
Jasmin Bom <jsmnbom@gmail.com>
2019-03-23 14:49:36 +0100

    Split and clean dictionary

--- src/js/officialDict.ts
+++ src/js/officialDict.ts
@@ -5,17 +5,17 @@ export class Word {
 
     constructor(raw: object) {
         // @ts-ignore
-        this.word = raw['word'].replace(/\s(\(e \))?$/g, '');
+        this.word = raw['tokipona'].replace(/\s(\(e \))?$/g, '');
         // @ts-ignore
         this.class = raw['class'].trim();
         // @ts-ignore
-        this.definitions = raw['definitions'].split(/[;,]/).map(def => def.trim());
+        this.definitions = raw['english'].split(/,/).map(def => def.trim());
     }
 }
 
 export async function getOfficialDict() {
     const raw = (await import("../data/official-dict.csv")).default;
-    return raw.map((rawWord: object) => new Word(rawWord)).filter((word: Word) => !(['separator', 'conjuction'].includes(word.class)));
+    return raw.map((rawWord: object) => new Word(rawWord));
 }
 
 export function formatWordClass(wordClass: string) {
